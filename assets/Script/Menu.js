5412f39035c9c7b1e82c19a8bcdadd711713f04f
jiangtao <jiangtao@tao-studio.net>
2018-07-07 15:11:40 +0800

    test loading

---
+++ assets/Script/Menu.js
@@ -0,0 +1,55 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+     onLoad () {
+         cc.log("enter Menu onLoad");
+         this.do_lot_of_this();
+
+         cc.log("exit Menu onLoad");
+     },
+
+    start () {
+        cc.log("enter Menu start");
+    },
+
+    do_lot_of_this:function(){      
+     
+    for(var i = 0; i < 100000; i++)
+    {
+        cc.log("i = " + i);
+    }
+     
+    }
+
+    // update (dt) {},
+});
